c9a18833fa6918b7642163e13a5c0a39193f60a9
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-09-25 16:13:05 -0500

    Mi primer commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://github.com/JohnFreddyTutistar" target="_blank">
+      <img src="https://www.pngarts.com/files/3/Naruto-Shippuden-PNG-Download-Image.png" class="logo" alt="Vite logo" />
+    </a>
+    
+    <h1>John Freddy Tutistar!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <h3 class="read-the-docs">
+      Bienvenido a mi página web
+    </h3>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
